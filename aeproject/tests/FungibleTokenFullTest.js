84cf0fcb94c3e23e8f6e937e0d3e4d9ade7fc9da
Anton.Pomieshchenko <anton.pomieshchenko@yalantis.net>
2022-09-17 22:01:40 +0300

    more tests

--- aeproject/tests/FungibleTokenFullTest.js
+++ aeproject/tests/FungibleTokenFullTest.js
@@ -6,6 +6,7 @@ const TOKEN_SOURCE = './contracts/FungibleTokenFull.aes';
 describe('FungibleTokenFull', () => {
   let aeSdk;
   let contract;
+  let original_amount = 1000000000;
 
   before(async () => {
     aeSdk = await utils.getSdk();
@@ -18,7 +19,7 @@ describe('FungibleTokenFull', () => {
 
     // initialize the contract instance
     contract = await aeSdk.getContractInstance({ source, fileSystem });
-    await contract.deploy(["USDT", 6, "USDT", 1000000000]);
+    await contract.deploy(["USDT", 6, "USDT", original_amount]);
 
     // create a snapshot of the blockchain state
     await utils.createSnapshot(aeSdk);
@@ -29,9 +30,41 @@ describe('FungibleTokenFull', () => {
     await utils.rollbackSnapshot(aeSdk);
   });
 
-  it('FungibleTokenFull: get owner balance', async () => {
+  it('get owner balance', async () => {
     const address = await utils.getDefaultAccounts()[0].address();
     const result = await contract.methods.balance(address);
-    assert.equal(result.decodedResult, 1000000000)
+    assert.equal(result.decodedResult, original_amount)
+  });
+
+  it('transfer', async () => {
+    const address = await utils.getDefaultAccounts()[0].address();
+    const another_address = await utils.getDefaultAccounts()[1].address();
+    const amount = 50000;
+
+    let result = await contract.methods.transfer(another_address, amount);
+    assert.equal(result.result.returnType, "ok");
+
+    result = await contract.methods.balances();
+    assert.equal(result.result.returnType, "ok");
+    assert.equal(result.decodedResult.get(address), original_amount - amount)
+    assert.equal(result.decodedResult.get(another_address), amount)
+  });
+
+  it('allowance -> transfer', async () => {
+    const address = await utils.getDefaultAccounts()[0].address();
+    const another_address = await utils.getDefaultAccounts()[1].address();
+    const amount = 50000;
+
+    let result = await contract.methods.create_allowance(another_address, amount);
+    assert.equal(result.result.returnType, "ok");
+
+    let accounts = utils.getDefaultAccounts();
+    result = await contract.methods.transfer_allowance(address, another_address, amount, {onAccount: accounts[1]});
+    assert.equal(result.result.returnType, "ok");
+
+    result = await contract.methods.balances();
+    assert.equal(result.result.returnType, "ok");
+    assert.equal(result.decodedResult.get(address), original_amount - amount)
+    assert.equal(result.decodedResult.get(another_address), amount)
   });
 });
