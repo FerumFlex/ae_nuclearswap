8cd02e97303f2fe6011ad32b2e24cfd3af1d6f5f
Anton.Pomieshchenko <anton.pomieshchenko@yalantis.net>
2022-09-18 00:19:22 +0300

    work in progress

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import { AppShell } from '@mantine/core';
 import { HeaderResponsive } from './Components/Header';
 import { FooterCentered } from './Components/Footer';
 import { Content } from './Components/Content';
+import { initSdk } from './utils/aeternity';
 
 function App() {
+  const [sdkReady, setSdkReady] = useState(false);
+  const [address, setAddress] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      const aeSdk = await initSdk();
+      setSdkReady(true);
+      const _address = await aeSdk.address();
+      setAddress(_address);
+      console.log(_address);
+    })();
+  }, []);
+
   return (
     <AppShell
       padding="md"
---
+++ src/src/utils/aeternity.ts
@@ -0,0 +1,46 @@
+import { AeSdkAepp, BrowserWindowMessageConnection, walletDetector, Node, SUBSCRIPTION_TYPES } from "@aeternity/aepp-sdk";
+
+const TESTNET_NODE_URL = 'https://testnet.aeternity.io';
+const MAINNET_NODE_URL = 'https://mainnet.aeternity.io';
+const COMPILER_URL = 'https://compiler.aepps.com';
+
+export const scanForWallet = async (aeSdk: AeSdkAepp) => {
+  return new Promise((resolve, reject) => {
+    if (!aeSdk) reject("Failed! SDK is not initialized");
+    const handleNewWallet = async ({ wallets, newWallet } : any) => {
+      alert(2);
+      const wallet = newWallet || Object.values(wallets)[0]
+      stopScan();
+      await aeSdk.connectToWallet(await wallet.getConnection());
+      await aeSdk.subscribeAddress(
+        // @ts-ignore
+        SUBSCRIPTION_TYPES.subscribe,
+        "current"
+      );
+      resolve(wallet);
+    };
+    const scannerConnection = new BrowserWindowMessageConnection();
+    const stopScan = walletDetector(
+      scannerConnection,
+      handleNewWallet
+    )
+  });
+};
+
+export const initSdk = async() => {
+  const aeSdk = new AeSdkAepp({
+    name: "aeapp-demo",
+    nodes: [
+      { name: 'testnet', instance: new Node(TESTNET_NODE_URL) },
+      { name: 'mainnet', instance: new Node(MAINNET_NODE_URL) },
+    ],
+    compilerUrl: COMPILER_URL,
+    onAddressChange: (p) => console.info("OnAddressChange", p),
+    onDisconnect: (p) => console.info("onDisconnect", p),
+    onNetworkChange: (p) => console.info("onNetworkChange", p)
+  });
+
+  await scanForWallet(aeSdk);
+
+  return aeSdk;
+}
