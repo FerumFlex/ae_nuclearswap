9be9f631984b3991147ca30876affb41a79f11d3
Anton.Pomieshchenko <ferumflex@gmail.com>
2023-05-12 13:00:20 +0300

    fixed sol tests

--- truffle/migrations/1_deploy_usdt.js
+++ truffle/migrations/1_deploy_usdt.js
@@ -1,16 +1,17 @@
 const GATE = artifacts.require("Gate");
 const USDT = artifacts.require("USDT");
 
-const AE_TOKEN_ADDRESS = process.env["AE_TOKEN_ADDRESS"];
+const AE_TOKEN_ADDRESS = process.env["AE_TOKEN_ADDRESS"] || "ak_2FYNeoVoxz2jCvaautM12HtuhXoRbA3ZtjfCtX98AaqpAK4rzM";
+const ORACLE_ADDRESS = process.env["ORACLE_ADDRESS"] || "0x65EadB2007Fe35291feA96D8246D569FEA2628c0";
 
 module.exports = async function (deployer, network) {
   let usdtAddress;
-  let ORACLE_ADDRESS;
+  let oracleAddress;
   if (network === "arbitrum") {
     usdtAddress = "0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9";
     oracleAddress = "0x8F918586255bF2CCA9405c3B64227a757efa0579";
   } else {
-    oracleAddress = process.env["ORACLE_ADDRESS"];
+    oracleAddress = ORACLE_ADDRESS;
     await deployer.deploy(USDT);
     let usdtInstance = await USDT.deployed();
     usdtAddress = usdtInstance.address;
